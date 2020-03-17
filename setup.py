50e87a428036264ead40b9bd0f27044da87b59c2
Edmund Higham <edhigham@gmail.com>
2020-03-17 18:02:26 +0000

    update setup.py for deps & include challenge.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
+with open('requirements.txt', encoding='utf8') as f:
+    requirements = f.read().splitlines()
+
 with open('README.rst', encoding="utf8") as f:
     readme = f.read()
 
@@ -9,12 +12,14 @@ with open('LICENSE', encoding="utf8") as f:
 
 setup(
     name='broad',
-    version='0.1.0',
+    version='0.1',
     description='Broad Institute Coding Challenge',
     long_description=readme,
     author='Edmund Higham',
     author_email='edhigham@gmai..com',
     url='https://github.com/ehigham/broad-challenge',
     license=license,
-    packages=find_packages(exclude=('test', 'doc'))
+    install_requires=requirements,
+    packages=find_packages(exclude=('test', 'doc')),
+    scripts=['challenge.py']
 )
