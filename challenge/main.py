b23bf49fefed704e0821db24ae461b9eb0a639b0
Edmund Higham <edhigham@gmail.com>
2020-03-17 00:20:26 +0000

    Implement dijkstra's algorithm over networkx

--- challenge/main.py
+++ challenge/main.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from collections import deque
+from heapq import (heappop, heappush)
+from sys import maxsize
 from typing import (Dict, List, Set, Tuple)
-from networkx import (MultiGraph, shortest_path)
+from networkx import (MultiGraph)
 
 from .functional import (compose, fmap, first, length, last, tail, zip_with)
 from .routes import (Stop, Route, load_routes, num_stops)
@@ -64,6 +67,54 @@ def __get_possible_route_segments(graph, path):
     make_journey_segment = lambda a, b: (a, make_route_set(a, b))
     return zip_with(make_journey_segment, path, tail(path))
 
+def dijkstra_shortest_path(graph, start, finish):
+    """
+    Implementation of dijkstra's famous shortest path algorithm.
+    """
+    # The bumf suggested we don't use a library to do everything for us, and
+    # so using networkx `shortest_path` is a little bit cheaky.
+
+    class Item(tuple):
+        """Used to define < (less than) for use with heapq"""
+        def __new__(cls, priority, vertex):
+            return tuple.__new__(Item, (priority, vertex))
+
+        def __lt__(self, rhs):
+            return first(self) < first(rhs)
+
+        def __repr__(self):
+            priority, vertex = self
+            return 'Item({:d}, {})'.format(priority, vertex)
+
+    distances = {vertex: maxsize for vertex in graph.nodes}
+    previous = {vertex: None for vertex in graph.nodes}
+    priority_queue = [Item(0, start)]
+
+    while priority_queue:
+        # pop the first vertex with the smallest distance
+        distance, vertex = heappop(priority_queue)
+
+        # only visit vertices we haven't seen yet
+        if distances[vertex] != maxsize:
+            continue
+
+        # visit all unseen neighbours and update their distance
+        distances[vertex] = distance
+        for neighbour in graph.neighbors(vertex):
+            if distances[neighbour] == maxsize:
+                heappush(priority_queue, Item(distance + 1, neighbour))
+                previous[neighbour] = vertex
+
+    # Backtrack through the `previous` list to build the path
+    path = deque()
+    while previous[finish]:
+        path.appendleft(finish)
+        finish = previous[finish]
+    if path:
+        path.appendleft(finish)
+
+    return list(path)
+
 def __print_change(stop, route):
     print("{} ({})".format(stop.name(), route.name() if route else ""))
 
@@ -72,7 +123,7 @@ def make_itinerary(routes, start, finish) -> List[Tuple[Stop, Route]]:
     Compute a path from start to finish and return a list of Changes
     """
     graph = __make_route_graph(routes)
-    path = shortest_path(graph, start, finish)
+    path = dijkstra_shortest_path(graph, start, finish)
     segments = __get_possible_route_segments(graph, path)
 
     current = None
@@ -113,7 +164,7 @@ def list_connections():
     route names for each of 'those stops'
     """
     connections = find_connecting_stops(load_routes())
-    for (stop, routes) in sorted(connections, key=compose(Stop.name, first)):
+    for stop, routes in sorted(connections, key=compose(Stop.name, first)):
         __print_connection_info(stop.name(), fmap(Route.name, routes))
 
 def plan_route(start, finish):
@@ -131,5 +182,5 @@ def plan_route(start, finish):
 
         return first(stops)
 
-    for (stop, route) in make_itinerary(routes, validate(start), validate(finish)):
+    for stop, route in make_itinerary(routes, validate(start), validate(finish)):
         __print_change(stop, route)
